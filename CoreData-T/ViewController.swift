cd55a183f0eb0a61ea6cd697b2244634a27e17cf
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-08-31 20:55:36 +0200

    Initial Commit

---
+++ CoreData-T/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData-T
+//
+//  Created by Andrew on 8/31/20.
+//  Copyright © 2020 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
